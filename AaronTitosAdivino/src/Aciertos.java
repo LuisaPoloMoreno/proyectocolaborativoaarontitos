e446331b70824ca08ee417978bc5313a8f27d1f2
Luisa Fernanda Polo Moreno <luisita021194@gmail.com>
2020-11-28 02:00:35 +0100

    correcciones añadidas

--- AaronTitosAdivino/src/Aciertos.java
+++ AaronTitosAdivino/src/Aciertos.java
@@ -1,12 +1,14 @@
+//Corregido por Luisa
 //Corregido por Vicente
 import java.util.Scanner;
 
 public class Aciertos {
 
     public static void main(String[] args) {
-        int n = 0,cont = 0;
+    	int n = 0,cont = 0;
         //int x = (int) (Math.random()*(10-50+1)+50); Esta instrucci�n no incluye ni al 10 ni al 50.
-        int x = (int) (Math.random()*((50-10+1)+10));// Esta si.
+        //int x = (int) (Math.random()*((50-10+1)+10));// Esta si.            //Luisa: éste cuenta hasta 50 pero empieza desde uno no desde 10
+        int x = (int) (Math.random()*41+10); // Luisa:Según la prueba que hice un poco más abajo, ésta seria una forma correcta de hacerlo.
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
@@ -20,6 +22,17 @@ public class Aciertos {
             cont++;
         } while (n != x);
         System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
-        sc.close();                        
+        sc.close();
+        
+        
+        
+        
+      
+        /*for(int i=1 ; i<=100; i++)
+        {
+        	int x = (int) (Math.random()*41+10);
+        	System.out.println(x);
+        } */
     }
 }
+
