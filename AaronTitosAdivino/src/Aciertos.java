afa4dbad32d76cb56c0a6bd8ac332c379133a254
Luisa Fernanda Polo Moreno <luisita021194@gmail.com>
2020-11-28 01:54:57 +0100

    Añadidos comentarios

--- AaronTitosAdivino/src/Aciertos.java
+++ AaronTitosAdivino/src/Aciertos.java
@@ -5,7 +5,7 @@ public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        //int x = (int) (Math.random()*(10-50+1)+50); Esta instrucción no incluye ni al 10 ni al 50.
+        //int x = (int) (Math.random()*(10-50+1)+50); Esta instrucciï¿½n no incluye ni al 10 ni al 50.
         int x = (int) (Math.random()*((50-10+1)+10));// Esta si.
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
@@ -20,6 +20,6 @@ public class Aciertos {
             cont++;
         } while (n != x);
         System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
-        sc.close();
+        sc.close();                        
     }
 }
